1a0d4b803ccd61ee8fba6355d979a3565542e60e
wxywr <1261046195@qq.com>
2017-09-21 20:25:19 +0800

    Update doctor.js

--- js/doctor.js
+++ js/doctor.js
@@ -59,8 +59,8 @@ $(function(){
     })
 
 	// 编辑分组
-    $(".edit").click(function (){
-		var edit = this;
+    $(".divide .edit").click(function (){
+		var edit = $(this).parent().prev();
 		var btnArray = ['取消','确定'];
 		mui.prompt('编辑分组名称','','',btnArray,function(e){
             if (e.index == 1) {
@@ -72,12 +72,12 @@ $(function(){
     })
 
   //   // 删除分组
-    $(".del").click(function (){
-    	var tmp = this;
+    $(".divide .del").click(function (){
+    	var de = $(this).parent().parent();
 		var btnArray = ['否', '是'];
         mui.confirm('', '确定删除该分组？<br/>所有患者将移至未分组', btnArray, function(e) {
             if (e.index == 1) {
-                tmp.remove();
+                de.remove();
             } else {
                 return;
             }
@@ -124,6 +124,27 @@ $(function(){
 
     $('.Add').click(function(){
         $('.allList').prepend('<div class="new"><div class="left"><img src="img/doctor.png" alt=""></div><div class="right"><div class="to"><input type="text" placeholder="姓名" class="do-name"><select class="do-job"><option value="">主任医师</option><option value="">主治医师</option></select><select class="team-job"><option value="">责任医师</option><option value="">团队医师</option></select></div><div class="mi"><p>简介:</p> <textarea name="" id="" rows="3"></textarea></div><div class="bo"><p>擅长:</p> <textarea name="" id="" rows="3"></textarea></div></div><div class="ok">保存</div></div>');
+    })
+
+
+    // 个人信息验证
+    $('.keep').click(function(){
+        var name = $('.username').val();
+        var check = $('input[name=sex]:checked').val();
+        var born = $('#demo1').val();
+        var phone = $('.iphone').val();
+        var place = $('.site').val();
+        var ill = $('.ago').val();
+        if(name==''||check==''||born==''||phone==''||place==''||ill==''){
+            $('.warn').text("请将信息填写完整！");
+            $('.warn').fadeIn().delay(1500).fadeOut();
+            return false;
+        }
+        if(!(/^1[34578]\d{9}$/.test(phone))){
+            $('.warn').text("手机号码填写有误！");
+            $('.warn').fadeIn().delay(1500).fadeOut();
+            return false;
+        }
     })
 
 })
